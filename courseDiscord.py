a6f0c08b6b8c5513e2ec9c0cbb3bdbf89667fee9
Matthew Cheung <70357013+Mcheung7272@users.noreply.github.com>
2021-03-30 22:15:24 -0400

    Added more features to the bot.
    I scraped all courses offered at UA (except a handful of subjects due to online formatting. I Will get to them soon!). Now students are able to see all subjects offered at UA, and lookup those specific courses from my SQLite database. Previously, students could only look up courses offered during the current semester and there was a small lookup time due to real-time scraping.

--- courseDiscord.py
+++ courseDiscord.py
@@ -3,9 +3,13 @@ from courseLookup import lookup, allInfo
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands, tasks
+import sqlite3
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+conn = sqlite3.connect(SQL DATABASE)
+c = conn.cursor()
 
 client = discord.Client()
 bot = commands.Bot(command_prefix='!')
@@ -56,11 +60,82 @@ async def look(ctx, s, n):
                         break
                     await ctx.channel.send(embed=embedVar)
 
+@bot.command(name='whatis')
+async def basicLookup(ctx, prefix, num):
+    if 'DROP' in prefix.upper() or '(' in prefix or ')' in prefix or '*' in prefix:
+        await ctx.send('No. Stop trying.')
+        return
+    elif 'DROP' in num.upper() or '(' in num or ')' in num or '*' in num:
+        await ctx.send('No. Stop trying.')
+        return
+    if prefix.upper() == 'AMAT':
+        prefix = 'MAT'
+    c.execute('SELECT * FROM ualbany_courses WHERE Subject_Prefix LIKE ? AND Subject_Number LIKE ?', ('%' + prefix.upper() + '%',num + '%',))
+    together = c.fetchone()
+    embedVar = discord.Embed(title="Course Lookup for " + prefix.upper() + ' ' + num, color=0x00ff00)
+    embedVar.add_field(name="Course Name", value=together[2] + ' | ' + together[0] + ' ' + together[1], inline=False)
+    embedVar.add_field(name="Course Description", value=together[3], inline=False)
+    embedVar.add_field(name="Subject", value=together[4], inline=False)
+    await ctx.send(embed=embedVar)
+        
+@bot.command(name='subjects')
+async def allSubjects(ctx):
+    prefix = []
+    f = open("prefix.txt", 'r')
+    for line in f:
+        prefix.append(line.replace('\n', ''))
+    c.execute('SELECT DISTINCT subject FROM ualbany_courses')
+    together = c.fetchall()
+    word = ''
+    for i in range(len(together)):
+        word = word + (str)(together[i]).replace('(','').replace(')', '').replace(',', '').replace("'", '').replace('"', '') + ' ' + (str)(prefix[i]).replace(',', '').replace("'", '').replace('"', '') + '\n'
+    await ctx.send(""" ```\nSubject and Prefix:\n \n{}\nSubjects Missing: Art, Communication, Global History, Music, Portuguese\n```""".format(word))
+
+@bot.command(name='coursesfor')
+async def coursesFor(ctx, subject):
+    if ctx.author.id == (int)('274354935036903424'):
+        await ctx.send('Go away James. Stop trying to drop my tables.')
+        return
+    if 'DROP' in subject.upper() or '(' in subject or ')' in subject or '*' in subject:
+        await ctx.send('No. Stop trying.')
+        return
+    c.execute('SELECT * FROM ualbany_courses WHERE Subject_Prefix = ?', (subject.upper(),))
+    together = c.fetchall()
+    if len(together) == 0:
+        await ctx.send('Zero Results. Make sure you searched with proper prefix. Use !subjects if you need it.')
+        return
+    skipped = []
+    for i in range(len(together)):
+        try:
+            embedVar = discord.Embed(title="Courses for " + subject.upper(), color=0x00ff00)
+            embedVar.add_field(name="Course Name, Prefix & Number:", value=together[i][2] + ' | ' + together[i][0] + ' | ' + together[i][1], inline=False)
+            embedVar.add_field(name="Course Description", value=together[i][3], inline=False) 
+            await ctx.send(embed=embedVar)
+        except:
+            skipped.append('Skipped lookup for ' + together[i][2] + ' | ' + together[i][0] + ' | ' + together[i][1] + '. Description too long to embed.')
+            continue
+    if(len(skipped) > 0):
+        for x in skipped:
+            await ctx.send(x)
+    
 @bot.command()
 async def helpme(ctx):
     embedVar = discord.Embed(color=0x00ff00)
     embedVar.add_field(name="!look" ,value="<Subject 'CSI'> <Course Number '201'> ")
     await ctx.channel.send(embed=embedVar)
+
+@basicLookup.error
+async def basicLookupError(ctx,error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Sorry, Bad arguments. Please try again. Make sure you search by proper prefix and number. Use !subjects if you need the proper prefix.')
+    if isinstance(error, commands.CommandError):
+        await ctx.send("Sorry, couldn't find the class. Make sure you search by proper prefix and number. Use !subjects if you need the proper prefix.")
+
+@coursesFor.error
+async def coursesForerror(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Sorry, Bad arguments. Please try again. Make sure you search by proper prefix and number. Use !subjects if you need the proper prefix.')
+
 
 
 
